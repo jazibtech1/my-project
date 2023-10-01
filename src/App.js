a96ac9cc34a38343c7cab0500043a675a6905ca1
Muhammad jazib ali <jazibalisher@gmail.com>
2023-10-01 12:53:22 -0700

    my first app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+
+    <div>
+<h1>hy am jazib</h1>
+
+<img src="2nature.jpeg" alt="Girl in a jacket" width="500" height="600"></img>
+</div>
+  );
+}
+
+export default App;
